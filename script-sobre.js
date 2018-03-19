9b8170a5d7c58d971f9e7e970096f4bb3b9f5f4f
Alexander Tuma <alexander.tuma@uds.com.br>
2018-03-19 16:40:35 -0300

    atualizado 19/03

--- script-sobre.js
+++ script-sobre.js
@@ -1,20 +1,20 @@
-var imageCount = 0;
-var images = new Array();
+var imagemAtual = 0;
+var imagens = new Array();
 
-images[0] = 'Fotos\grafite.jpg';
-images[1] = 'Fotos\parque do povo.jpg';
-images[2] = 'Fotos\foto terno.jpg';
-images[3] = 'Fotos\sesi chef.jpg';
+imagens[0] = 'Fotos/grafite.jpg';
+imagens[1] = 'Fotos/parque do povo.jpg';
+imagens[2] = 'Fotos/foto terno.jpg';
+imagens[3] = 'Fotos/sesi chef.jpg';
 
 //Aqui apenas adicione mais imagens
 function troca(){
-  if (imageCount < images.length-1){
-    imageCount++;
+  if (imagemAtual < imagens.length-1){
+    imagemAtual++;
   }else{
-    imageCount = 0
+    imagemAtual = 0
   }
   $(".imagem-local").fadeOut(500);
-  setTimeout("$('.imagem-local').html('<img src=\""+images[imageCount]+"\" width=\"100%\" height=\"100%\" />');$('.imagem-local').fadeIn(500);",500);
+  setTimeout("$('.imagem-local').html('<img src=\""+imagens[imagemAtual]+"\"/>');$('.imagem-local').fadeIn(500);",500);
 }
 var segundos = 2; //Segundos entre cada imagem
 setInterval("troca();",segundos*1000);
--- template.js
+++ template.js
@@ -4,10 +4,12 @@ if('content' in document.createElement('template')){
   var itemMenu = document.querySelector('#itemMenu'), li = itemMenu.content.querySelector("li");
   var ul = document.querySelector("ul");
 
-  li.innerHTML = "<a href='index.html'> Home </a>";
+  li.innerHTML = "<a href='home.html'> Home </a>";
   var clone = document.importNode(itemMenu.content, true);
   ul.appendChild(clone);
 
+//li.innerHTML = "<a href='Sobre.html' class='menu-ativo'> Sobre </a>";
+//fazer if para deixar selecionado no menu o atual
   li.innerHTML = "<a href='Sobre.html'> Sobre </a>";
   var clone2 = document.importNode(itemMenu.content, true);
   ul.appendChild(clone2);
